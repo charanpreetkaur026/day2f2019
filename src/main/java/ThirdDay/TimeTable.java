bcd9b52badde8a0faf14d188c10c413a28c69b4d
CHARAN <charanpreetkaur026@gmail.com>
2019-09-22 20:12:04 -0400

    string handling(Time Table)

--- src/main/java/ThirdDay/TimeTable.java
+++ src/main/java/ThirdDay/TimeTable.java
@@ -10,48 +10,52 @@ import java.time.Month;
 import java.time.Period;
 import java.util.Date;
 
-public class TimeTable
-{
-    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-    public static void main(String[] args)
-    {
-        //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-       // LocalDate localDate = LocalDate.now();
+public class TimeTable {
+    private static final DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy ");
 
+    public static void main(String[] args) {
         System.out.println("MADT F2019  - Class Timetable Design ");
         System.out.println("---------------------------------------\n");
         Date currentDate = new Date();
+        String saturdayOff = "Saturday";
+        String sundayOff = "Sunday";
         SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE"); // the day of the week spelled out completely
-        System.out.println(simpleDateformat.format(currentDate) + "\t" + dateFormat.format(currentDate));
+        if (simpleDateformat.format(currentDate) == sundayOff) {
+            System.out.println(simpleDateformat.format(currentDate) + ",\t" + dateFormat.format(currentDate) + "at 17:30 to 21:45");
 
-        // convert date to calendar
-        Calendar c = Calendar.getInstance();
-        c.setTime(currentDate);
-        int i=0;
-        do{
-            // manipulate date
-            c.add(Calendar.DATE, 1); //same with c.add(Calendar.DAY_OF_MONTH, 1);
-            // convert calendar to date
-            Date currentDatePlusOne = c.getTime();
-            System.out.println(simpleDateformat.format(currentDatePlusOne) +"\t" + dateFormat.format(currentDatePlusOne) );
-            i++;
-        }while(i<4);
+            // convert date to calendar
+            Calendar c = Calendar.getInstance();
+            c.setTime(currentDate);
+            int i = 0;
+            do {
+                c.add(Calendar.DATE, 1); //incrementing date by 1
+                Date currentDatePlusOne = c.getTime();
 
-        //DateTimeFormatter currentDateAndTime = currentDateAndTime.format(now); //2016/11/16 12:08:43
-       // LocalDateTime oldDate = LocalDateTime.of(2016, Month.AUGUST, 31, 10, 20, 55);
-        //LocalDateTime newDate = LocalDateTime.of(2016, Month.NOVEMBER, 9, 10, 21, 56);
-        //System.out.println(dtf.format(localDate)); //2016/11/16
-       // System.out.println("--- Examples --- ");
+                System.out.println(simpleDateformat.format(currentDatePlusOne) + ",\t" + dateFormat.format(currentDatePlusOne) + "at 17:30 to 21:45");
+                i++;
+                //(simpleDateformat.format(currentDatePlusOne) != sundayOff )
+            } while (i < 4);
+        } else {
+            Calendar c = Calendar.getInstance();
+            c.setTime(currentDate);
+            int i = 0;
+            do {
+                c.add(Calendar.DATE, 1); //incrementing date by 1
+                Date currentDatePlusOne = c.getTime();
 
-        //Period tenDays = Period.ofDays(10);
-        //System.out.println(tenDays.getDays()); //10
+                System.out.println(simpleDateformat.format(currentDatePlusOne) + ",\t" + dateFormat.format(currentDatePlusOne) + "at 17:30 to 21:45");
+                i++;
+                //(simpleDateformat.format(currentDatePlusOne) != sundayOff )
+            } while (i < 5);
 
-        Period oneYearTwoMonthsThreeDays = Period.of(1, 2, 3);
-        //System.out.println(oneYearTwoMonthsThreeDays.getYears());   //1
-        //System.out.println(oneYearTwoMonthsThreeDays.getMonths());  //2
-        //System.out.println(oneYearTwoMonthsThreeDays.getDays());    //3
+
+            //Period tenDays = Period.ofDays(10);
+            //System.out.println(tenDays.getDays()); //10
+
+            Period oneYearTwoMonthsThreeDays = Period.of(1, 2, 3);
+            //System.out.println(oneYearTwoMonthsThreeDays.getYears());   //1
+            //System.out.println(oneYearTwoMonthsThreeDays.getMonths());  //2
+            //System.out.println(oneYearTwoMonthsThreeDays.getDays());    //3
 
         /*System.out.println("\n--- Period.between --- ");
         LocalDate oldDate = LocalDate.of(2019,Month.SEPTEMBER,20);
@@ -61,30 +65,20 @@ public class TimeTable
         System.out.println(currentDateAndTime);
         //LocalDateTime mondayDate = LocalDateTime.of();
         Duration d1 = Duration.between(oldDate, newDate);
-        System.out.println("hello");
-        System.out.println(d1.toString());*/
 
-
-
-
-
-
-
-
-    }
-        //System.out.println(oldDate);
-        //System.out.println(newDate);
+        System.out.println(oldDate);
+        System.out.println(newDate);
 
         // check period between dates
-        /*Period period = Period.between(oldDate, newDate);
+        Period period = Period.between(oldDate, newDate);
 
         System.out.print(period.getYears() + " years,");
         System.out.print(period.getMonths() + " months,");
         System.out.print(period.getDays() + " days");*/
 
 
-
+        }
     }
-
+}
 
 
